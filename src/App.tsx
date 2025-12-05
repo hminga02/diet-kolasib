92c69d67392b7e3821d5a206a326fe9394d05248
hminga010-boop <hminga010@gmail.com>
2025-12-05 18:43:19 +0530

    Update05 App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import AdminDashboard from "./pages/AdminDashboard";
 import NotFound from "./pages/NotFound";
